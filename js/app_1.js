b115a7cd5044780e60d63e9634de1c18c54cfd35
FrancescaD <frankied@gwmail.gwu.edu>
2015-03-31 20:03:38 -0400

    Update app_1.js

--- js/app_1.js
+++ js/app_1.js
@@ -2,7 +2,7 @@
 // Enter your mapbox map id here to reference it for the base layer
 
 var mapId = 'francescad.360854ec'; //<- this references the ugly green map that I made
-var token = 'pk.eyJ1Ijoid2lsbC1icmVpdGtyZXV0eiIsImEiOiItMTJGWEF3In0.HEvuRMMVxBVR5-oDYvudxw'; //<- this is my token, use yours.
+var token = 'pk.eyJ1IjoiZnJhbmNlc2NhZCIsImEiOiIwVDBpMlNzIn0.wlV7e5_p8nupd8rTCyi9VQ'; //<- this is my token, use yours.
 
 //Create the map object with your mapId and token
 L.mapbox.accessToken = token;
